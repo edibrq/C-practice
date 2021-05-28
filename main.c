a87748713fc79753925a5227a8f683f9ca0b5e84
Eduard <eduardibr04@gmail.com>
2021-05-28 14:58:18 +0300

    Sample of simple code

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void add(int* a, int* b){
+    *a+=1;
+    *b+=1;
+}
+
 int main() {
     printf("Hello, World!\n");
+    int a = 5;
+    int b = 6;
+    printf("a = %d,\nb = %d\n",a,b);
+    add(&a,&b);
+    printf("Values now is:\n");
+    printf("a = %d,\nb = %d",a,b);
     return 0;
 }
