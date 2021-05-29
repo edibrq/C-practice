04c62ec1f3555e3b85a258467c1c26571c654d37
Eduard <eduardibr04@gmail.com>
2021-05-29 13:47:27 +0300

    Save the result of my suffering

--- main.c
+++ main.c
@@ -7,13 +7,6 @@ struct node{
     struct node* next; //pointer with address of the next node
 };
 
-void push(struct node** head, int x){ //insertion to the beginning
-    struct node* temp = (struct node*) malloc(sizeof(struct node)); //creating a node
-    temp->data = x;
-    temp->next = *head;
-    *head = temp;
-}
-
 void print(struct node** head){
     struct node* temp = *head;
     printf("The List is:");
@@ -24,23 +17,31 @@ void print(struct node** head){
     printf("\n");
 }
 
+void push(struct node** head, int x){ //insertion to the beginning
+    struct node* temp = (struct node*) malloc(sizeof(struct node)); //creating a node
+    temp->data = x;
+    temp->next = *head;
+    *head = temp;
+}
+
 void insert(struct node** head, int n, int value){
     struct node* temp = (struct node*)malloc(sizeof(struct node));
     temp->data = value;
     temp->next = NULL;
 
     if(n == 1){ //when list is empty
-        temp->next = *head;
+       // temp->next = *head;
         *head = temp;
         return;
     }
-
+    printf("*head:%d", *head);
     struct node* iter = *head;
-    for (int i = 0; i < n - 2; ++i) {
+    printf("iter.next:%d", iter -> next);
+    for (int i = 0; i < n - 2; ++i) { //n - 1 is the first node there
         iter = iter->next; //goto n - 1 element of the list
     }
-    temp->next = iter->next;
-    iter->next = temp;
+    temp->next = iter->next; // creating link to the next element
+    iter->next = temp; // creating link from n - 1 element to nth element
 }
 
 void pop(struct node** head, int x){
@@ -48,35 +49,32 @@ void pop(struct node** head, int x){
     temp->data = x;
     struct node* iter = *head;
     while(iter != NULL){
-        if(iter->next == NULL)
-            temp->next = iter;
+    }
+    temp->next = iter->next;
+}
+
+void delete(struct node** head, int n, int value){
+    if(n == 1){
+        free(*head);
+        struct node* temp;
+        for (int i = 0; i < n + 1; ++i) {
+            temp = temp->next;
+        }
+        *head = temp;
+        return;
     }
 
+
+    for (int i = 0; i < n + 1; ++i) {
+
+    }
 }
 
 int main() {
-
     struct node* head = NULL;
-
-//    printf("How many elements?\n");
-//    int n,x;
-//    scanf("%d",&n);
-//    for (int i = 0; i < n; ++i) {
-//        printf("Enter the number:\n");
-//        scanf("%d",&x);
-//        pop(&head, x);
-//        print(&head);
-//    }
-
-    insert(&head, 1, 2);
+    insert(&head,1,3);
+    insert(&head,2,5);
     print(&head);
-    insert(&head, 2, 3);
-    print(&head);
-    insert(&head, 3, 4);
-    print(&head);
-
-
-
 }
 
 
